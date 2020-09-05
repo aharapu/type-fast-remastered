d16a28cfba9bb6e565fa5a62d9b946ca4eb47144
Alexandru-Valentin Harapu <aharapu2@yahoo.com>
2020-09-05 21:22:08 +0300

    first files

---
+++ src/components/layout.js
@@ -0,0 +1,12 @@
+// use layout component to repeat the layout across different pages.
+// this can include header, footer, nav etc.
+
+import React from "react"
+
+export default function Layout({ children }) {
+  return (
+    <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
+      {children}
+    </div>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,15 @@
 import React from "react"
 import { Link } from "gatsby"
 import Header from "../components/header"
+import Layout from "../components/layout"
 
 export default function Home() {
   return (
-    <div style={{ color: `purple` }}>
+    <Layout>
       <Link to="/contact/">Contact</Link>
       <Header headerText="Hello Gatsby!" />
       <p>What a world.</p>
       <img src="https://source.unsplash.com/random/400x200" alt="" />
-    </div>
+    </Layout>
   )
 }

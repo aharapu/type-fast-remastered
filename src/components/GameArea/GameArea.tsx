c0b6d883e7a20a7a53ec87aacb85b56a3a2892e9
Alexandru-Valentin Harapu <aharapu2@yahoo.com>
2020-09-07 11:32:01 +0300

    basic layout

--- src/components/GameArea/GameArea.tsx
+++ src/components/GameArea/GameArea.tsx
@@ -1,16 +1,92 @@
-import React from "react"
-import { Container, Row, Col } from "react-bootstrap"
-import * as styles from './GameArea.module.scss'
+import React, { useState } from "react"
+import {
+  Container,
+  Row,
+  Col,
+  ToggleButtonGroup,
+  ToggleButton,
+  Button,
+  InputGroup,
+  FormControl
+} from "react-bootstrap"
+import * as styles from "./GameArea.module.scss"
 
 const GameArea: React.FC = () => {
+  const [value, setValue] = useState([1, 3])
+  const handleChange = val => setValue(val)
   return (
-    <Container className={styles.gamearea}>
-      <Row>
-        <Col xs={12} sm={10} md={8}>
-          <h2 className="text-center m-auto">Choose Level</h2>
-        </Col>
-      </Row>
-    </Container>
+    <>
+      <br />
+      <Container>
+        <Row>
+          <Col
+            xs={12}
+            sm={{ offset: 1, span: 10 }}
+            md={{ offset: 2, span: 8 }}
+            className={styles.gamearea}
+          >
+            <Container>
+              <br />
+              <Row>
+                <Col xs={12} className="text-center">
+                  <h3>Choose Level:</h3>
+                </Col>
+              </Row>
+              <Row>
+                <Col xs={12} className="text-center">
+                  <ToggleButtonGroup
+                    className="btn-group-sm"
+                    name="difficulty"
+                    type="radio"
+                    value={value}
+                    onChange={handleChange}
+                  >
+                    <ToggleButton value={1}>Noobie</ToggleButton>
+                    <ToggleButton value={2}>Normal</ToggleButton>
+                    <ToggleButton value={3}>Hardcore</ToggleButton>
+                  </ToggleButtonGroup>
+                </Col>
+              </Row>
+              <br />
+              <Row>
+                <Col xs={12} className="text-center">
+                  <Button size="lg" disabled={true}>START</Button>
+                </Col>
+              </Row>
+              <br />
+              <Row>
+                <Col xs={12} className="text-center">
+                  <p style={{ whiteSpace: "pre" }}>
+                    Type this: <span id="first">This is some </span>
+                    <span id="second">palceholder text.</span>
+                  </p>
+                </Col>
+              </Row>
+              <Row>
+                <Col xs={12} className="text-center">
+                  <InputGroup size="sm" className="mb-3">
+                    <InputGroup.Prepend>
+                      <InputGroup.Text id="inputGroup-sizing-sm">
+                        Your text:
+                      </InputGroup.Text>
+                    </InputGroup.Prepend>
+                    <FormControl aria-label="Small" aria-describedby="inputGroup-sizing-sm" />
+                  </InputGroup>
+                </Col>
+              </Row>
+              <br />
+              <Row>
+                <Col xs={12} className="text-center">
+                  <p style={{ whiteSpace: "pre" }}>
+                    Time left: <span id="time">20</span>s
+                  </p>
+                </Col>
+              </Row>
+            </Container>
+          </Col>
+        </Row>
+      </Container>
+    </>
   )
 }
 
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,15 @@
-import React from "react"
-import { Navbar, Nav } from "react-bootstrap"
+import React, { useState } from "react"
+import { Navbar, Nav, Button, Modal, Table } from "react-bootstrap"
 import { Link } from "gatsby"
 
 const InfoBar: React.FC = () => {
+  const [areInstructionsShown, setAreInstructionsShown] = useState(false)
+  const [areHighScresShown, setAreHighScresShown] = useState(false)
+
+  const handleInsClose = () => setAreInstructionsShown(false)
+  const handleInsShow = () => setAreInstructionsShown(true)
+  const handleHighClose = () => setAreHighScresShown(false)
+  const handleHighShow = () => setAreHighScresShown(true)
   return (
     <Navbar bg="light" expand="sm">
       <Link to="/">
@@ -11,10 +18,58 @@ const InfoBar: React.FC = () => {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse>
         <Nav className="ml-auto">
-          <Nav.Link active>High Scores</Nav.Link>
-          <Nav.Link active>Instructions</Nav.Link>
+          <Nav.Link active onClick={handleHighShow}>
+            High Scores
+          </Nav.Link>
+          <Nav.Link active onClick={handleInsShow}>
+            Instructions
+          </Nav.Link>
         </Nav>
       </Navbar.Collapse>
+      <Modal show={areInstructionsShown} onHide={handleInsClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Instructions</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Put some instructions here!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleInsClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      <Modal show={areHighScresShown} onHide={handleHighClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>High Scores</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Player Name</th>
+                <th>Score</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>Mark</td>
+                <td>400</td>
+              </tr>
+              <tr>
+                <td>2</td>
+                <td>Jacob</td>
+                <td>399</td>
+              </tr>
+            </tbody>
+          </Table>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleHighClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </Navbar>
   )
 }
